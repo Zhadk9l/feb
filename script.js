6fc96501412c71012f4caa916a8b40ec865a47c7
Zhadk9l <zhadko.danylo@lll.kpi.ua>
2024-02-10 21:10:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleText() {
+    var textBlock = document.getElementById("textBlock");
+    if (textBlock.style.display === "none") {
+        textBlock.style.display = "block";
+        document.getElementById("toggleButton").innerText = "✉️";
+    } else {
+        textBlock.style.display = "none";
+        document.getElementById("toggleButton").innerText = "✉️";
+    }
+}
