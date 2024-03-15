10df84f1d76250045d7ccd92711cd0cee80be96b
Zhadk9l <zhadko.danylo@lll.kpi.ua>
2024-03-15 23:56:18 +0200

    end_end

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function toggleText() {
     var textBlock = document.getElementById("textBlock");
     if (textBlock.style.display === "none") {
         textBlock.style.display = "block";
-        document.getElementById("toggleButton").innerText = "✉️";
+        document.getElementById("toggleButton").innerText = "📧";
     } else {
         textBlock.style.display = "none";
-        document.getElementById("toggleButton").innerText = "✉️";
+        document.getElementById("toggleButton").innerText = "📧";
     }
 }
